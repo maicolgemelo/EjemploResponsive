bf0e7b4bcbe668861b3b7578325909506ed590fb
Michael Escobar Galeano <maicolgemelo@gmail.com>
2019-11-24 13:22:39 -0500

    proyecto el cual implemento reponsive design para organizar las paginas web, organizando el diseño  dependiendo de la resolucion

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Ejemplo página Responsive Design
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <div id="pagewrap">
+        <div id="header">
+            <div id="info">
+                    <h1>Black friday</h1>
+                    <p>Aprovecha nuestras promociones de fines de semana.</p>
+            </div>
+            <div id="logo">
+                    <img class="imagen1" src="imagenes/balckfriday.png">
+            </div>
+            
+            
+        </div>
+
+        <div id="content">
+            <h2>Contenido</h2>
+            <p>Párrafo 1</p>
+            <p>Párrafo 2</p>
+            <p>Párrafo 3</p>
+            <p>Párrafo 4</p>
+            <p>Párrafo 5</p>
+            <p>Párrafo 6</p>          
+        </div>
+
+        <div id="sidebar">
+            <h3>Sidebar</h3>
+            <p>Texto 1</p>
+            <p>Texto 2</p>
+            <p>Texto 3</p>
+        </div>
+        <div id="Footer">
+            <h4>Footer</h4>
+        </div>
+
+    </div>
+</body>
+</html>
